5f1fe5a8cebd95b7ff4ed4bad2054d1c34deec41
sajieun <sajieun000721@naver.com>
2024-07-04 00:16:14 +0900

    보안 수정

--- src/main/java/com/example/cookie/service/UserService.java
+++ src/main/java/com/example/cookie/service/UserService.java
@@ -28,6 +28,10 @@ public class UserService {
                 // cookie 해당 정보를 저장
                 cookie = new Cookie("authorization-cookie", userDto.getId());
                 cookie.setDomain("localhost"); // naver.com , daum.net
+                // 밑에 두개는 업무할때 필수!
+                cookie.setHttpOnly(true); // 검사에서 cookie 값을 읽을 수 없도록 만들어준다.
+                cookie.setSecure(true); // https 에서만 쿠기가 되게끔 하겠다.
+
                 cookie.setPath("/");
                 cookie.setMaxAge(-1); // 연결된 동안만 사용하겠다.
 
