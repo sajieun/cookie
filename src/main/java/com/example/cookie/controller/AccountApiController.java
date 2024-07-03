1e916daadf6b40d67f68b1f5c5343e394b11d587
sajieun <sajieun000721@naver.com>
2024-07-03 23:07:10 +0900

    login.html -> index.html rename

--- src/main/java/com/example/cookie/controller/AccountApiController.java
+++ src/main/java/com/example/cookie/controller/AccountApiController.java
@@ -4,10 +4,7 @@ import com.example.cookie.model.LoginRequest;
 import com.example.cookie.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/account")
@@ -16,7 +13,7 @@ public class AccountApiController {
 
     private final UserService userService;
 
-    @PostMapping("/login")
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     public void login(
             @RequestBody
             LoginRequest loginRequest,
