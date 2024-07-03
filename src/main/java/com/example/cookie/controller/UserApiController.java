112081b6443e49396d49674fa0c3e93ad7768619
sajieun <sajieun000721@naver.com>
2024-07-03 23:42:40 +0900

    userApi create log

---
+++ src/main/java/com/example/cookie/controller/UserApiController.java
@@ -0,0 +1,35 @@
+package com.example.cookie.controller;
+
+import com.example.cookie.db.UserRepository;
+import com.example.cookie.model.UserDto;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/user")
+public class UserApiController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/me")
+    public UserDto me(
+            HttpServletRequest httpServletRequest
+    ){
+        var cookies = httpServletRequest.getCookies();
+        if (cookies != null){
+            for (Cookie cookie: cookies){
+                log.info("key : {}, value : {}", cookie.getName(), cookie.getValue());
+            }
+        }
+
+        return null;
+    }
+}
